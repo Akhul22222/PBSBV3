6451e7abd6788ee91d55fcaf26f0a22d1401803f
Akhul22222 <85484061+Akhul22222@users.noreply.github.com>
2021-06-08 03:05:23 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You Can't Control me I am @parkboyschat Family 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@parkboyschat
 </i>"""    
     
     HELP_TEXT = """
